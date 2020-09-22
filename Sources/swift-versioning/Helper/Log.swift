8a76c78e371db5de3db7e20860c869ff7c1f13bf
nihilias <nihilias@icloud.com>
2020-09-22 22:05:08 +0200

    feat: add verbose logging

---
+++ Sources/swift-versioning/Helper/Log.swift
@@ -0,0 +1,16 @@
+//
+// Created by NanoGiants GmbH on 22.09.20.
+// Copyright © 2020 NanoGiants GmbH. All rights reserved.
+//
+
+import Foundation
+
+public struct Log {
+    static var verbose: Bool = false
+    
+    func verbose(_ items: Any...) {
+        if Log.verbose {
+            print(items)
+        }
+    }
+}
