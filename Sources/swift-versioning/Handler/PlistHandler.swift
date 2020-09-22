6b2445ac3403ae63aa0e7d129d3b61b62581bc8f
nihilias <nihilias@icloud.com>
2020-09-22 23:44:10 +0200

    feat: WIP reading and writing plist

--- Sources/swift-versioning/Handler/PlistHandler.swift
+++ Sources/swift-versioning/Handler/PlistHandler.swift
@@ -19,9 +19,12 @@ final class PlistHandler: PlistHandlerProtocol {
         return encoder
     }
     
+    private var decoder = PropertyListDecoder()
+    
     // MARK: - Methode
     
     func write(_ version: Version, to plistPathString: String) {
+        Log.verbose("Write version:")
         if plistPathString.isValid() {
             let plistUrl: URL = URL(fileURLWithPath: plistPathString, isDirectory: false)
             save(version, to: plistUrl)
@@ -33,17 +36,50 @@ final class PlistHandler: PlistHandlerProtocol {
     // MARK: - Private Methods
     
     private func save(_ version: Version, to plistUrl: URL) {
-        if let data = try? encoder.encode(version) {
-            do {
-                // TODO: writing data to plist will overwrite content -> don't
+        do {
+            if let plistData = read(plistUrl) {
+                let versionData = try encoder.encode(version)
+                let data = NSMutableData()
+                data.append(plistData)
+                data.append(versionData)
+                
+                Log.verbose("... beginn writing to plist.")
                 try data.write(to: plistUrl, options: .atomic)
+                Log.verbose("... done!")
+            } else {
+                // TODO: error handling -> could read plist
+                Log.verbose("Error: couldn't fetch plist")
+            }
+        } catch {
+            // TODO: error handling -> could not write data
+            Log.verbose("Error: couldn't read plist")
+        }
+    }
+    
+    private func read(_ plistUrl: URL) -> Data? {
+        Log.verbose("... reading existing plist.")
+        if let data = try? Data(contentsOf: plistUrl) {
+            do {
+                let tmpDict = try PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: nil)
+                Log.verbose("... done!: \(tmpDict)")
+                    
+//                if let tmpDict = try PropertyListSerialization.propertyList(from: data, options: .mutableContainers, format: nil) as? Data {
+//                    Log.verbose("... done!: \(tmpDict)")
+//                    return nil
+//                } else {
+//                    // TODO: error handling -> couldn't cast propertyList to data
+//                    Log.verbose("Error: couldn't cast propertyList to data")
+//                }
             } catch {
-                // TODO: error handling -> could not write data
+                // TODO: error handling -> couldn't decode existing plist
+                Log.verbose("Error: couldn't decode existing plist")
             }
         } else {
-            // TODO: error handling -> could not encode version
-            print("could not encode version")
+            // TODO: error handling -> couldn't read plist
+            Log.verbose("Error: couldn't read plist")
         }
+        
+        return nil
     }
 }
 
