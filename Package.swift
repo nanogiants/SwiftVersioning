464e757a9df947e29bea4f57e85c618a4382adb8
nihilias <nihilias@icloud.com>
2020-09-22 21:54:40 +0200

    feat: add Resolver for dep injection

--- Package.swift
+++ Package.swift
@@ -5,16 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "swift-versioning",
+    platforms: [
+        .macOS(.v10_14)
+    ],
     products: [
         .executable(name: "swift-versioning", targets: ["swift-versioning"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
+        .package(url: "https://github.com/hmlongco/Resolver.git", from: "1.1.4")
     ],
     targets: [
         .target(
             name: "swift-versioning", dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Resolver", package: "Resolver"),
         ]),
         .testTarget(
             name: "swift-versioningTests", dependencies: ["swift-versioning"]),
