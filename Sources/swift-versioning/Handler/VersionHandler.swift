fefc794d4b21bc2c33d1fb9eea496830886c04cc
Marius Hötten-Löns <m.hoetten-loens@nanogiants.de>
2020-09-25 13:59:47 +0200

    refactor: setup of version retrieval

--- Sources/swift-versioning/Handler/VersionHandler.swift
+++ Sources/swift-versioning/Handler/VersionHandler.swift
@@ -6,68 +6,100 @@
 import Foundation
 
 protocol VersionHandlerProtocol {
-    var version: String { get }
-    var branch: String { get }
-    
+    var version: String? { get }
     var major: String? { get }
     var minor: String? { get }
     var patch: String? { get }
-    var attachments: String? { get }
     var build: String { get }
+
+    var attachments: String? { get }
+    var branch: String { get }
 }
 
 final class VersionHandler: VersionHandlerProtocol {
     // MARK: - Properties
     
-    public var version: String { tag }
-
-    public var major: String? {
-        if !tagBits.isEmpty {
-            return tagBits[0]
-        }
-
-        return nil
+    var version: String?
+    var major: String?
+    var minor: String?
+    var patch: String?
+    var build: String {
+        launch(command: tool.command, arguments: tool.buildArguments)
     }
 
-    public var minor: String? {
-        if tagBits.count > 1 {
-            return tagBits[1]
-        }
-
-        return nil
+    var attachments: String?
+    var branch: String {
+        launch(command: tool.command, arguments: tool.branchArguments)
     }
-
-    public var patch: String? {
-        if tagBits.count > 2 {
-            return tagBits[2].components(separatedBy: "_").first
-        }
-
-        return nil
-    }
-
-    public var attachments: String? {
-        if tagBits.count > 2 {
-            var attArray = tagBits[2].components(separatedBy: "_")
-            attArray.removeFirst()
-            return attArray.joined(separator: "_")
-        }
-
-        return nil
-    }
-
-    public var build: String { launch(command: tool.command, arguments: tool.buildArguments) }
-    public var branch: String { launch(command: tool.command, arguments: tool.branchArguments) }
 
     // MARK: - Private Properties
 
-    private var tag: String { launch(command: tool.command, arguments: tool.tagArguments) }
-    private var tagBits: [String] { tag.components(separatedBy: ".") }
+    private var tag: String {
+        launch(command: tool.command, arguments: tool.tagArguments)
+    }
 
+    private var tagBits: [String]?
+    private var isReleaseCandidate: Bool = false
     private var tool: VersionControlSystem
 
     // MARK: - Init
 
     init(for tool: VersionControlSystem) {
         self.tool = tool
+
+        bootstrap()
+    }
+
+    // MARK: - Private Methods
+
+    private func bootstrap() {
+        setupVersion()
+        setupSemantics()
+        setupAttachments()
+    }
+
+    private func setupVersion() {
+        version = tag
+        if branch.contains("release/") || branch.contains("hotfix/") {
+            version = versionFromBranch()
+            isReleaseCandidate = true
+        }
+
+        tagBits = version?.components(separatedBy: ".")
+    }
+
+    // TODO: for the love of god, use regex
+    private func versionFromBranch() -> String {
+        branch
+            .components(separatedBy: "_")[0]
+            .components(separatedBy: "/")[1]
+    }
+
+    private func setupSemantics() {
+        if let tagBits = tagBits {
+            major = !tagBits.isEmpty ? tagBits[0] : nil
+            minor = tagBits.count >= 2 ? tagBits[1] : nil
+            patch = tagBits.count >= 3 ? tagBits[2] : nil
+        } else {
+            Log.error("Unable to retrieve semantics from tag.")
+        }
+    }
+
+    private func setupAttachments() {
+        let releaseString: String = isReleaseCandidate ? "release" : "feature"
+
+        var branchInfoString: String = ""
+        var branchInfoStrings: [String] = branch
+            .components(separatedBy: "/")[1]
+            .components(separatedBy: "_")
+        branchInfoStrings.append(releaseString)
+        if let possibleVersionString = branchInfoStrings.first {
+            if possibleVersionString == version ?? "" {
+                branchInfoStrings.removeFirst()
+            }
+        }
+
+        branchInfoString = branchInfoStrings.joined(separator: "_")
+        attachments = "\(branchInfoString)"
     }
 }
--- Sources/swift-versioning/Models/Version.swift
+++ Sources/swift-versioning/Models/Version.swift
@@ -6,7 +6,7 @@
 import Foundation
 
 struct Version: Codable {
-    var version: String
+    var version: String?
     var branch: String
     var major: String?
     var minor: String?
